8157fa839d3538ad80d59ffced2ed48dbed2f6b6
Ivan Dubovik <vanekby98@gmail.com>
2021-08-30 18:48:05 +0300

    feat: add role_user to user and add admin

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -66,6 +66,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function __construct()
     {
+        $this->setRoles(['ROLE_USER']);
         $this->OwnCollection = new ArrayCollection();
         $this->comments = new ArrayCollection();
     }
